62be768ff058a820c2a8318cf9e45627089b15f1
Trey Guy <tguy728@gmail.com>
2019-09-26 03:03:06 +0000

    devise issues fixed

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,7 +1,6 @@
 Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'localhost:3030'}
 
-end
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
