ee98281f2937165b96688d6f31915ef43e0018cd
Trey Guy <tguy728@gmail.com>
2019-09-26 01:12:06 +0000

    Devise issues

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,8 @@
 Rails.application.configure do
+  config.action_mailer.default_url_options = { host: 'flixter-trey-guy.herokuapp.com'}
   # Settings specified here will take precedence over those in config/application.rb.
+
+end
 
   # Code is not reloaded between requests.
   config.cache_classes = true
