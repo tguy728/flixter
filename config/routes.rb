3fa3e27836b4e87b9eb53547b04b513da0045cd7
Trey Guy <tguy728@gmail.com>
2019-09-27 02:54:43 +0000

    simple form issue

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'static_pages#index'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :instructor do
+    resources :courses, only: [:new, :create, :show]
+  end
 end
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
