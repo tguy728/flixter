a4f8fcecbe496acb38a8405fc2e1b8c4ce582595
Trey Guy <tguy728@gmail.com>
2019-09-30 21:28:14 +0000

    routes issues in lessons controller

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,4 +1,7 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_authorized_for_current_lesson, only: [:show]
+    redirect_to course_path(current_course), alert: 'Not currently enrolled in lesson'
   def show
   end
 
