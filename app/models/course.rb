c411b4dc5691859585e55d8eabd8d1b7f2fd0a53
Trey Guy <tguy728@gmail.com>
2019-09-30 19:58:18 +0000

    added enrollment button enhancement and n+query for enrollment array lookup

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,9 +1,12 @@
 class Course < ApplicationRecord
+ mount_uploader :image, ImageUploader 
+
+
   belongs_to :user
   has_many :sections
+  has_many :enrollments
 
-  mount_uploader :image, ImageUploader
-
+  
   validates :title, presence: true
   validates :description, presence: true
   validates :cost, presence: true, numericality: {greater_than_or_equal_to: 0}
