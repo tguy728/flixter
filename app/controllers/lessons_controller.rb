d06dfba90dc92083adb523e1a45d4146f8d20e94
Trey Guy <tguy728@gmail.com>
2019-10-01 02:20:22 +0000

    stripe undefined method error

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,21 +1,21 @@
 class LessonsController < ApplicationController
   before_action :authenticate_user!
   before_action :require_authorized_for_current_section
-
-  def require_authorized_for_current_section
-    if current_user.enrolled_in? presence: true
-      else render plain: "Unauthorized", status: :unauthorized
-          redirect_to course_path(current_lesson.section.course), alert: 'Not currently enrolled in lesson'
-  end
     
   def show
   end
 
   private
 
-  helper_method :current_lesson
-  def current_lesson
-    @current_lesson ||= Lesson.find(params[:id])
+    helper_method :current_lesson
+    def require_authorized_for_current_section
+      if current_user.enrolled_in? presence: true
+        render plain: "Unauthorized", status: :unauthorized
+            redirect_to course_path(current_lesson.section.course), alert: 'Not currently enrolled in lesson'
+    end
+    
+    def current_lesson
+      @current_lesson ||= Lesson.find(params[:id])
+    end
   end
-end
 end
